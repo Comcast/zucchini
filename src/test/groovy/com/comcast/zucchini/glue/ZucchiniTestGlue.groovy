87b8c59d78c28e9cdb27d710df3a41ed6493ad13
Clark Malmgren <clark_malmgren@cable.comcast.com>
2014-10-08 13:43:57 -0700

    Fix package names and add additional license and informatinal files

--- src/test/groovy/com/comcast/zucchini/glue/ZucchiniTestGlue.groovy
+++ src/test/groovy/com/comcast/zucchini/glue/ZucchiniTestGlue.groovy
@@ -1,9 +1,24 @@
-package com.comcast.csv.zucchini.glue
+/**
+ * Copyright 2014 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.comcast.zucchini.glue
 
 import org.testng.Assert
 
-import com.comcast.csv.zucchini.TestContext
-import com.comcast.csv.zucchini.Veggie
+import com.comcast.zucchini.TestContext
+import com.comcast.zucchini.Veggie
 
 import cucumber.api.java.en.And
 import cucumber.api.java.en.Given
